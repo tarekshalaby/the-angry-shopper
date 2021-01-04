7b503b8a52c47958832d8506de8b552d9b153391
Tarek Shalaby <tarek@shala.by>
2021-01-04 15:27:39 -0600

    Built the browse page via the routes and view, starting with the latest price changes

--- theangryshopper/routes.py
+++ theangryshopper/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template, redirect, url_for, request
+from sqlalchemy import or_
 from theangryshopper import app, db
 from theangryshopper.models import Categories, GourmetProducts, GourmetCategories, MetroProducts
+
 
 @app.route('/')
 def home():
 	return redirect('/compare')
+
 
 @app.route('/compare')
 def compare_latest():
@@ -13,7 +16,9 @@ def compare_latest():
 	query = db.session.query(GourmetProducts).filter(GourmetProducts.id.in_(latest), GourmetProducts.price > 0)
 	limit = 50
 	products = query.order_by(GourmetProducts.updated.desc()).limit(limit).all()
+
 	return render_template('compare.html', products=products, count=limit, categories=categories) 
+
 
 @app.route('/compare/<category>')
 def compare_category(category):
@@ -24,23 +29,27 @@ def compare_category(category):
 	query = db.session.query(GourmetProducts).filter(GourmetProducts.id.in_(latest), GourmetProducts.price > 0)
 	products = query.order_by(GourmetProducts.title.asc())
 	count = query.count()
+
 	return render_template('compare.html', active_category=active_category, products=products, count=count, categories=categories, path=path) 
+
 
 @app.route('/browse/')
 def browse():
 	return ("Browse")
 
-supermarket_class = {'gourmet' : GourmetProducts, 'metro': MetroProducts}
 
 @app.route('/browse/<supermarket>')
 def browse_supermarket_latest(supermarket):
+	supermarket_class = {'gourmet' : GourmetProducts, 'metro': MetroProducts}
 	target = supermarket_class[supermarket]
+
 	categories = db.session.query(Categories).order_by(Categories.title.asc())
-	latest = db.session.query(db.func.max(target.id)).group_by(target.product_id).all()
-	query = db.session.query(target).filter(target.id.in_(latest), target.price > 0)
-	limit = 50
-	products = query.order_by(target.updated.desc()).limit(limit).all()
-	changed = db.session.query(GourmetProducts.product_id).group_by(GourmetProducts.product_id).having(db.func.max(GourmetProducts.price) != db.func.min(GourmetProducts.price)).all()
-	latest_changed = db.session.query(GourmetProducts.id).filter(GourmetProducts.id.in_(latest), GourmetProducts.product_id.in_(changed)).all()
-	test = db.session.query(GourmetProducts.id, GourmetProducts.product_id, GourmetProducts.title, GourmetProducts.price).filter(GourmetProducts.product_id.in_(changed)).order_by(GourmetProducts.product_id.asc()).all()
-	return render_template('browse.html', products=products, count=limit, categories=categories, test=test) 
+
+	ultimate_ids = db.session.query(db.func.max(target.id).label('max_id')).group_by(target.product_id).all()
+	penultimate_ids = db.session.query(db.func.max(target.id)).filter(~target.id.in_(ultimate_ids), target.price > 0).group_by(target.product_id).all()
+	ultimate_prices = db.session.query(target).filter(target.id.in_(ultimate_ids), target.price > 0).subquery()
+	penultimate_prices = db.session.query(target.id, target.product_id, target.price, target.updated).filter(target.id.in_(penultimate_ids)).order_by(target.updated.desc()).subquery()
+
+	products = db.session.query(ultimate_prices, penultimate_prices.c.updated.label('days'), (((ultimate_prices.c.price - penultimate_prices.c.price)/penultimate_prices.c.price)*100).label('difference')).join(penultimate_prices, ultimate_prices.c.product_id == penultimate_prices.c.product_id).filter(ultimate_prices.c.price != penultimate_prices.c.price).limit(50).all()
+
+	return render_template('browse.html', categories=categories, products=products) 
