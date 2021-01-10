b1ca87aa6ed37240ffadf72c578062f78ba61fd3
Tarek Shalaby <tarek@shala.by>
2021-01-09 19:00:56 -0600

    Changed around the configuration to generically access the database locally and in the production environment

--- theangryshopper/__init__.py
+++ theangryshopper/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
+from theangryshopper import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("SQLALCHEMY_DATABASE_URI")
+app.config['SQLALCHEMY_DATABASE_URI'] = config.alchemy_configuration
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
